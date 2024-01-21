5f42241c925be0581d490f8dfe990abc19cd0ae6
Darwis <107021419+darwisrh@users.noreply.github.com>
2024-01-21 23:17:59 +0700

    update

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ import { Express, Request, Response } from "express"
 const app: Express = express()
 const port: Number = 3000
 
-app.get("/test", (req: Request, res: Response) => {
-   res.render("Hello world")
+app.get("/", (_: Request, res: Response) => {
+   res.send(`<h1 style="font-size: 60px;">Hello world</h1>`)
 })
 
 app.listen(port, () => {
-   console.log(`Server run at : localhost:${port}`)   
+   console.log(`Server run at : http://localhost:${port}`)   
 })
